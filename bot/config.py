863cdb645008b84249db1b9544eb98ce46eb888f
servicebyoop <102507731+servicebyoop@users.noreply.github.com>
2022-05-01 14:01:13 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,14 +3,14 @@ import json
 from distutils.util import strtobool as stb
 
 # --------------------------------------
-BOT_TOKEN = ""
-GDRIVE_FOLDER_ID = ""
+BOT_TOKEN = "5160909424:AAEUF-UsTTG0_DkvuUJxlcE0gDZsq0ZWGOg"
+GDRIVE_FOLDER_ID = "1-0r89K5o8SgmsqwzTr9BYfbUpFgzibYW"
 # Default folder id.
-OWNER_ID = 123455673
+OWNER_ID = 769748540
 # Example: OWNER_ID = 619418070
 AUTHORISED_USERS = []
-# Example: AUTHORISED_USERS = [63055333, 100483029, -1003943959]
-INDEX_URL = ""
+# Example: AUTHORISED_USERS = [769748540 , -1001476314218]
+INDEX_URL = "https://mi.opgohilservice.workers.dev/0:"
 IS_TEAM_DRIVE = True
 USE_SERVICE_ACCOUNTS = True
 # --------------------------------------
